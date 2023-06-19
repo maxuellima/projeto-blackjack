e3183bafa056bf4b4303cb7913e530627af2ac65
Maxuel Lima <maxuellima20@gmail.com>
2023-06-19 15:15:36 -0300

    Versão 1 e 2 do projeto BlackJack

--- README.md
+++ README.md
@@ -26,25 +26,25 @@ O enunciado completo está no Sistema de Estudante, na sessão de Projetos https
 ## Lista de requisitos
 
 
-- [ ] O usuário deve encontrar uma mensagem de boas vindas no console ao abrir o programa
+- [X] O usuário deve encontrar uma mensagem de boas vindas no console ao abrir o programa
 	
-- [ ] O usuário deve ser questionado se deseja iniciar uma nova rodada	
+- [X] O usuário deve ser questionado se deseja iniciar uma nova rodada	
 
-- [ ] Se o usuário recusar iniciar uma nova rodada, o programa deve imprimir uma mensagem informando o fim do jogo	
+- [X] Se o usuário recusar iniciar uma nova rodada, o programa deve imprimir uma mensagem informando o fim do jogo	
 
-- [ ] Se o usuário aceitar iniciar uma nova rodada, o programa deve exibir as cartas do jogador e do computador
+- [X] Se o usuário aceitar iniciar uma nova rodada, o programa deve exibir as cartas do jogador e do computador
 
-- [ ] Ao final do jogo, o programa deve imprimir o resultado informando quem ganhou	
+- [X] Ao final do jogo, o programa deve imprimir o resultado informando quem ganhou	
 
-- [ ] Implementou a verificação para impedir um par de A na primeira mão	
+- [X] Implementou a verificação para impedir um par de A na primeira mão	
 
-- [ ] O jogo esconde a uma das cartas do computador ao início da rodada	
+- [X] O jogo esconde a uma das cartas do computador ao início da rodada	
 
-- [ ] O usuário é questionado se deseja comprar mais cartas	
+- [X] O usuário é questionado se deseja comprar mais cartas	
 
-- [ ] Se o usuário somar mais de 21, o mesmo perde automaticamente	
+- [X] Se o usuário somar mais de 21, o mesmo perde automaticamente	
 
-- [ ] Se o computador somar mais de 21, o mesmo perde automaticamente	
+- [X] Se o computador somar mais de 21, o mesmo perde automaticamente	
 
 
 
--- versao_2.js
+++ versao_2.js
@@ -10,4 +10,81 @@
  * 
  * 
  */
+alert(`Bem vindo ao novo jogo de cartas Blackjack 2.0!`)
+let start = confirm(`Deseja iniciar a partida?`)
 
+let arrayPlayer = []
+let arrayComputer = []
+let cardsPlayer = ``
+let cardsComputer = ``
+let somaPlayer = 0
+let somaComputer = 0
+let winner = ""
+
+if (start) {
+   let buyCards = true
+   while (buyCards) {
+      for (i = 0; i <= 1; i++) {
+         arrayPlayer.push(comprarCarta())
+         arrayComputer.push(comprarCarta())
+         somaPlayer += arrayPlayer[i].valor
+         somaComputer += arrayComputer[i].valor
+         cardsPlayer += arrayPlayer[i].texto + " "
+         cardsComputer += arrayComputer[i].texto + " "
+         if (somaPlayer === 22 || somaComputer === 22) {
+            somaPlayer = 0
+            somaComputer = 0
+            for (i = 0; i <= 1; i++) {
+               arrayPlayer.push(comprarCarta())
+               arrayComputer.push(comprarCarta())
+               somaPlayer += arrayPlayer[i].valor
+               somaComputer += arrayComputer[i].valor
+               cardsPlayer += arrayPlayer[i].texto + " "
+               cardsComputer += arrayComputer[i].texto + " "
+            }
+         } else {
+            buyCards = false
+         }
+      }
+   }
+
+   let compraDeCartas = true
+   while (compraDeCartas) {
+      if (somaPlayer > 21) {
+         compraDeCartas = false;
+      }
+      else {
+         let novaRodada = confirm(`Suas cartas são: ${cardsPlayer}\nCarta revelada do computador é: ${arrayComputer[0].texto}\nDeseja comprar mais carta?`)
+         if (novaRodada) {
+            arrayPlayer.push(comprarCarta())
+            cardsPlayer += arrayPlayer[arrayPlayer.length - 1].texto + " "
+            somaPlayer += arrayPlayer[arrayPlayer.length - 1].valor
+            console.log(`Cartas do jogador: ${cardsPlayer}`)
+            console.log(`Pontos do jogador ${somaPlayer}`)
+         }
+         else {
+            compraDeCartas = false
+         }
+      }
+   }
+   if (somaPlayer <= 21) {
+      while (somaComputer < 21 && somaComputer <= somaPlayer) {
+         arrayComputer.push(comprarCarta())
+         somaComputer += arrayComputer[arrayComputer.length - 1].valor
+         cardsComputer += arrayComputer[arrayComputer.length - 1].texto + " "
+      }
+   }
+   console.log(`Cartas finais do jogador: ${cardsPlayer}\nCarta finais do computador: ${cardsComputer}`)
+   console.log(`Pontos jogador ${somaPlayer}\nPontos computador: ${somaComputer}`)
+
+   if (somaComputer <= 21 && (somaComputer > somaPlayer || somaPlayer > 21)) {
+      winner = `O computador venceu!`
+   } else if (somaPlayer <= 21 && (somaPlayer > somaComputer || somaComputer > 21)) {
+      winner = "O jogador venceu!"
+   } else {
+      winner = "Empate!"
+   }
+   alert(`Cartas do jogador: ${cardsPlayer} - Pontos do jogador: ${somaPlayer}\nCartas computador: ${cardsComputer} - Pontos do Computador: ${somaComputer}\n${winner}`)
+} else {
+   alert(`Você não iniciou o jogo!`)
+}
